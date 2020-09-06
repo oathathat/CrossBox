54aee02af74f88604f910d7b7b19091379130438
User <User@OAT-PC>
2020-09-06 23:00:17 +0700

    Wrong

--- Project1/BoxInFrame.cpp
+++ Project1/BoxInFrame.cpp
@@ -1,17 +1,22 @@
 #include<stdio.h>
-int main() {
+main() {
     int i, j;
     int n;
     scanf_s("%d", &n);
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= n; j++) {
-            if (i + j <= n / 2 + 1 || j - i >= n / 2 + 1 || i - j >= n / 2 + 1 || i + j > 2 * n - (n / 2))
+            if (j == 2 && i != 1 && i != n)
+                printf(" ");
+            else if (j == n - 1 && i != 1 && i != n)
+                printf(" ");
+            else if (i == 2 && j != 1 && j != n)
+                printf(" ");
+            else if (i == n - 1 && j != 1 && j != n)
                 printf(" ");
             else
                 printf("*");
         }
         printf("\n");
     }
-
     return 0;
 }
